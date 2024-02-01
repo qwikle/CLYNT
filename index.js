d2e2c1215a49e976564b54e78d9a0c36f1194082
Qwikle <qwikle.ziko@gmail.com>
2024-02-01 17:07:30 +0100

    refactor: execute yarn after creating the new project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import program from './src/cmd/commander.js';
 import prompt from 'prompts';
 import chalk from 'chalk';
+import {exec} from 'child_process';
 import boxen from 'boxen';
 import { licencesPrompt, useDefaultLayer, ListOfSpecifiLayers } from './src/cmd/prompt.js';
 import {  createRootWorkspaceJson, buildLayers, createFolder } from './src/cmd/builder.js';
@@ -41,17 +42,24 @@ if (options.new) {
   process.chdir(options.new); 
   await createRootWorkspaceJson({name: options.new, author, licence,layers});
   await buildLayers({names: layers, author});
-  console.log(boxen(`
-  Workspace created ${chalk.greenBright('successfully')}!
-  run ${chalk.cyanBright(`cd ${options.new} && yarn start`)}
-  if ${layers[1]} depends on ${layers[0]} 
-  add in the ${layers[1]} package.json "dependencies" the following:
-  ${chalk.cyanBright(`"${layers[0]}": "*"`)}
-  And so on...
-  ${chalk.cyanBright('Have fun!')}
-  `,
-    {borderColor: "cyan", title: `${options.new}`, titleAlignment: "center", padding: 1, margin: 1, borderStyle:"double"}));
+  exec('yarn start', (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(boxen(`
+    Workspace created ${chalk.greenBright('successfully')}!
+    run ${chalk.cyanBright(`cd ${options.new}`)}
+    if ${layers[1]} depends on ${layers[0]} 
+    add in the ${layers[1]} package.json "dependencies" the following:
+    ${chalk.cyanBright(`"${layers[0]}": "*"`)}
+    And so on...
+    ${chalk.cyanBright('Have fun!')}
+    `,
+      {borderColor: "cyan", title: `${options.new}`, titleAlignment: "center", padding: 1, margin: 1, borderStyle:"double"}));
+  })
 }
+console.log(`installing dependencies...`)
 if (options.add) {
   console.log('add');
 }
